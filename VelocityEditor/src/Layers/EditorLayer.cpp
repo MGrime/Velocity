5b7667e2b08da4f824c3c234ad63382d72c1d184
Michael Grime <MGrime1@uclan.ac.uk>
2021-01-10 15:32:25 +0000

    Ready to start testing lighting

--- VelocityEditor/src/Layers/EditorLayer.cpp
+++ VelocityEditor/src/Layers/EditorLayer.cpp
@@ -15,21 +15,22 @@ void EditorLayer::OnAttach()
 
 	// Prepare camera
 	m_CameraController = std::make_unique<DefaultCameraController>();
-	m_CameraController->GetCamera()->SetPosition({ 0.0f,3.0f,3.0f });
-	m_CameraController->GetCamera()->SetRotation({ -50.0f,0.0f,0.0f });
+	m_CameraController->GetCamera()->SetPosition({ 0.0f,5.0f,0.0f });
+	m_CameraController->GetCamera()->SetRotation({ -90.0f,0.0f,0.0f });
 	m_Scene->SetCamera(m_CameraController->GetCamera().get());
 
 	// Load Mesh
-	renderer->LoadMesh("assets/models/room.obj","Room");
+	renderer->LoadMesh("assets/models/pbrsphere.obj","Sphere");
 
 	// Load texture
-	renderer->CreateTexture("assets/textures/room.png", "Room");
+	renderer->CreateTexture("assets/textures/wood.jpg", "Wood");
 
 	m_Skybox = std::unique_ptr<Skybox>(renderer->CreateSkybox("assets/textures/skyboxes/Park2", ".jpg"));
 
-	auto room = m_Scene->CreateEntity("Room");
-	room.AddComponent<MeshComponent>(Renderer::GetRenderer()->GetMesh("Room"));
-	room.AddComponent<TextureComponent>(TextureComponent{ Renderer::GetRenderer()->GetTextureByReference("Room") });
+	auto room = m_Scene->CreateEntity("Sphere");
+	room.GetComponent<TransformComponent>().Rotation.x = 90.0f;
+	room.AddComponent<MeshComponent>(Renderer::GetRenderer()->GetMesh("Sphere"));
+	room.AddComponent<TextureComponent>(TextureComponent{ Renderer::GetRenderer()->GetTextureByReference("Wood") });
 
 	m_Scene->SetSkybox(m_Skybox.get());
 }
